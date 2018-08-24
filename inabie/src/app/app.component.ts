15da482f9b9c7ecb82e114bb03a5961cec5a1fc8
Pablo Ismael Sanchez Rijo <pablo17sanchez@hotmail.com>
2018-08-24 14:12:24 -0400

    AddingHomePage

--- inabie/src/app/app.component.ts
+++ inabie/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Home2Page } from './../pages/home2/home2';
+import { HomePage } from './../pages/home/home';
+
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
+//import { Home2Page } from "../pages/home2/home2";
 import { ListPage } from '../pages/list/list';
-
+import { Component, ViewChild } from "@angular/core";
 @Component({
   templateUrl: 'app.html'
 })
@@ -18,10 +19,10 @@ export class MyApp {
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
+//
+    // used for an example of ngFor and navigation { title: 'List', component: ListPage }
     this.pages = [
-      { title: 'Home', component: HomePage },
+      // { title: 'Inicio ', component: HomePage },
       { title: 'List', component: ListPage }
     ];
 
@@ -35,7 +36,10 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
-
+  inicio(){
+   
+    this.nav.push(Home2Page);
+  }
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
--- inabie/src/app/app.module.ts
+++ inabie/src/app/app.module.ts
@@ -1,34 +1,26 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+//pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { Home2Page } from "../pages/home2/home2";
+import { PagesFooterPage } from "../pages/pages-footer/pages-footer";
+//elements
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
+  declarations: [MyApp, HomePage, ListPage, PagesFooterPage, Home2Page],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
+  entryComponents: [MyApp, HomePage, ListPage, Home2Page],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
